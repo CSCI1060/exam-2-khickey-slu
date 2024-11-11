301d73230a909f95b792b8bc050d14c42bded419
khickey-slu <kevin.hickey@slu.edu>
2024-11-11 00:24:17 +0000

    Add files via upload

--- fruit.m
+++ fruit.m
@@ -1,7 +1,23 @@
-function [] = fruit( N )
+% Kevin Hickey
+
+function [] = fruit(N)
 % This function prints for values from 1 to N:
 % “apple” if the value is divisible by 2
 % “banana” if the value is divisible by 5
 % The number itself if neither is true
 % “applebanana” if both are true
+    for i = 1:N
+        if rem(i, 2) == 0
+            fprintf("apple")
+        end
 
+        if rem(i, 5) == 0
+            fprintf("banana")
+        end
+        
+        if rem(i, 2) ~= 0 && rem(i, 5) ~= 0
+            fprintf("%d", i)
+        end
+        fprintf("\n")
+    end
+end
--- longestWord.m
+++ longestWord.m
@@ -1,4 +1,6 @@
-function [result] = longestWord( inputFile )
+% Kevin Hickey
+
+function [result] = longestWord(inputFile)
 %
 % inputFile specifies the file whose characters should
 % be analyzed.
@@ -14,3 +16,36 @@ function [result] = longestWord( inputFile )
 % print a descriptive error message and return the
 % value -1.
 %
+    fileID = fopen(inputFile, 'r');
+
+    if fileID == -1
+        -1
+        error("There is no file with that name to analyze. Please try a different name.")
+    end
+
+    streak = 0;
+    longest_streak = 0;
+
+    while 1
+        input = fread(fileID, 1);
+        if length(input) == 0
+            if streak > longest_streak
+                longest_streak = streak;
+            end
+
+            break
+        end
+        
+        if input == '?' || input == '.' || input == ' ' || input == '!' ...
+        || input == '''' || input == ',' || input == 10
+            if streak > longest_streak
+                longest_streak = streak;
+            end
+            streak = 0;
+        else
+            streak = streak + 1;
+        end 
+    end
+
+    result = longest_streak;
+end
